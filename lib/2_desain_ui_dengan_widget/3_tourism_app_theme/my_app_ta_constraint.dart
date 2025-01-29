6918c65fe685224926a0ed569e6040b9a6d8e4d5
Yosuarman <reyyosua29@gmail.com>
2025-01-29 18:35:06 +0800

    tourism app tema

--- lib/2_desain_ui_dengan_widget/3_tourism_app_theme/my_app_ta_constraint.dart
+++ lib/2_desain_ui_dengan_widget/3_tourism_app_theme/my_app_ta_constraint.dart
@@ -2,15 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:training/2_desain_ui_dengan_widget/3_tourism_app_theme/model/tourism.dart';
 import 'package:training/2_desain_ui_dengan_widget/3_tourism_app_theme/screen/detail/detail_screen.dart';
 import 'package:training/2_desain_ui_dengan_widget/3_tourism_app_theme/screen/home/home_screen.dart';
+import 'package:training/2_desain_ui_dengan_widget/3_tourism_app_theme/static/navigation_route.dart';
 
-import '2_desain_ui_dengan_widget/3_tourism_app_theme/static/navigation_route.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAppTAConstraint extends StatelessWidget {
+  const MyAppTAConstraint({super.key});
 
   @override
   Widget build(BuildContext context) {
