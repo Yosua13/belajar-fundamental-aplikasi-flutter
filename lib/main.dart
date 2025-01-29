bdb6a5e780debd0b908c36400799d3b78b810f01
Yosuarman <reyyosua29@gmail.com>
2025-01-29 20:11:06 +0800

    hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:training/2_desain_ui_dengan_widget/2_tourism_app_constraint/my_app_ta_constraint.dart';
+import 'package:training/2_desain_ui_dengan_widget/3_tourism_app_theme/my_app_ta_constraint.dart';
 
 void main() {
   runApp(const MyAppTAConstraint());
